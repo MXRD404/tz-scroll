00ee2f4c9f010d71563de53e3ea73e1f43fc0483
MXRD <mxrd@MXRDs-MacBook-Pro.local>
2024-12-03 19:32:01 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, EventEmitter, Output, signal } from '@angular/core';
+import { InViewportDirective } from './viewport.directive';
+import { NgClass, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [InViewportDirective, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tz-scroll';
+  isFirstBorder = signal(false)
+  isSecondBorder = signal(false)
+  counter = signal<number>(0)
+
+  constructor() { }
+
+  onVisibleSecondScroll(isVisible: boolean) {
+    if (this.isSecondBorder()) return;
+    if (isVisible) {
+      this.isSecondBorder.set(true)
+    }
+  }
+
+  onVisibleFirstScroll(isVisible: boolean) {
+    this.isFirstBorder.set(isVisible)
+    if (isVisible) {
+      this.counter.update((v) => v + 1)
+    }
+  }
 }
---
+++ src/app/viewport.directive.ts
@@ -0,0 +1,27 @@
+import { Directive, ElementRef, EventEmitter, Input, Output } from '@angular/core';
+
+@Directive({
+  selector: '[appInViewport]',
+  standalone: true
+})
+export class InViewportDirective {
+  @Output() visible = new EventEmitter<boolean>();
+  private observer: IntersectionObserver;
+
+  constructor(private element: ElementRef) {
+    this.observer = new IntersectionObserver(
+      ([entry]) => {
+        this.visible.emit(entry.isIntersecting);
+      },
+      { threshold: 0.1 }
+    );
+  }
+
+  ngOnInit() {
+    this.observer.observe(this.element.nativeElement);
+  }
+
+  ngOnDestroy() {
+    this.observer.disconnect();
+  }
+}
