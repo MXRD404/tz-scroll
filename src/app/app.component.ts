7ab57f235bec9849b1ef6d43dd8d9f6a7a849f5e
MXRD <mxrd@MXRDs-MacBook-Pro.local>
2024-12-03 20:42:36 +0300

    Рефакторинг

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,40 @@
-import { Component, ElementRef, EventEmitter, OnDestroy, Output, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { InViewportDirective } from './viewport.directive';
-import { NgClass, NgStyle } from '@angular/common';
-import { Subscription } from 'rxjs';
+import { NgClass } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
+
+enum BorderEnum {
+  firstBorder = 1,
+  secondBorder = 2
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [InViewportDirective, NgClass],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isFirstBorder = signal(false)
-  isSecondBorder = signal(false)
-  counter = signal<number>(0)
+  currentBorder = new BehaviorSubject<BorderEnum | undefined>(undefined);
+  counter = signal<number>(0);
+  secondBorderVisible = false;  // Флаг, чтобы рамка второго бордера оставалась активной
 
+  constructor() {}
 
-  constructor() { }
-
-  onVisibleSecondScroll(isVisible: boolean) {
-    if (this.isSecondBorder()) return;
-    if (isVisible) {
-      this.isSecondBorder.set(true)
+  onVisibleScroll(border: BorderEnum, isVisible: boolean) {
+    if (border === BorderEnum.secondBorder && isVisible && !this.secondBorderVisible) {
+      this.secondBorderVisible = true; 
+      this.currentBorder.next(BorderEnum.secondBorder); 
     }
-  }
 
-  onVisibleFirstScroll(isVisible: boolean) {
-    this.isFirstBorder.set(isVisible)
-    if (isVisible) {
-      this.counter.update((v) => v + 1)
+    if (border === BorderEnum.firstBorder && isVisible && this.currentBorder.value !== BorderEnum.firstBorder) {
+      this.currentBorder.next(BorderEnum.firstBorder);  
+      this.counter.update(v => v + 1); 
+    }
+
+    if (!isVisible && border === BorderEnum.firstBorder) {
+      this.currentBorder.next(undefined);
     }
   }
 }
